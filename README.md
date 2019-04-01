7940115faa90a01ad985881f67d77338391b529c
jlpospisil <josh.pospisil@gmail.com>
2019-04-01 14:39:36 -0500

    Initialized storybook

---
+++ README.md
@@ -0,0 +1,18 @@
+Getting Started
+---
+
+Install dependencies:
+
+```
+npm install
+```
+
+Starting Storybook
+---
+    npm run storybook
+    
+    npm run bundler
+    
+    npm run ios
+        OR
+    npm run android
---
+++ src/tests/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App from '../../App';
+
+/* eslint-disable-next-line no-undef */
+it('renders without crashing', () => {
+  const rendered = renderer.create(<App />).toJSON();
+  expect(rendered).toBeTruthy();
+});
