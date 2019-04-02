9b2c6f2187758fe1c479846fac8c70b28e2565b7
jlpospisil <josh.pospisil@gmail.com>
2019-04-02 10:26:53 -0500

    Added eslint and linted code Added jest and single test

--- storybook/index.js
+++ storybook/index.js
@@ -1,10 +1,11 @@
 import { AppRegistry } from 'react-native';
+/* eslint-disable-next-line import/no-extraneous-dependencies */
 import { getStorybookUI, configure } from '@storybook/react-native';
 import './rn-addons';
 
 // import stories
 configure(() => {
-  require('./stories');
+  require('./stories'); // eslint-disable-line global-require
 }, module);
 
 // Refer to https://github.com/storybooks/storybook/tree/master/app/react-native#start-command-parameters
--- storybook/stories/lists/simple.js
+++ storybook/stories/lists/simple.js
@@ -1,4 +1,5 @@
 import React from 'react';
+/* eslint-disable-next-line import/no-extraneous-dependencies */
 import { storiesOf } from '@storybook/react-native';
 import { storybookContainer } from '../../decorators';
 import { SimpleList } from '../../../src/components';
